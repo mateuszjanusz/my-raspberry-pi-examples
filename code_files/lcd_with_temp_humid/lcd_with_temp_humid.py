5ca0cffef10faae56641ff7f8ff333f0b286bc6b
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-08-04 21:23:45 +0100

    fix for lcd print

--- code_files/lcd_with_temp_humid/lcd_with_temp_humid.py
+++ code_files/lcd_with_temp_humid/lcd_with_temp_humid.py
@@ -11,7 +11,6 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
-    my_lcd.lcd_display_string('Temp: {0:0.1f} C'.format(temperature), 1)
-    my_lcd.lcd_display_string('Humidity: {1:0.1f} %'.format(humidity), 2)
+    my_lcd.lcd_display_string('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity), 1)
 
 
